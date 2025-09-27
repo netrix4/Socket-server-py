a3600f01f3565599f4712bf01de8cb3387f9314b
Mario <netrix4@gmail.com>
2025-09-26 23:50:55 -0700

    base code upgraded

--- client.py
+++ client.py
@@ -1,13 +1,26 @@
 import socket
-HOST = '192.168.0.190'
+# HOST = '192.168.0.190'
+# HOST = '0.0.0.0'
+HOST = '192.168.100.79'
 PORT = 555
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
     print(f"Conectado al servidor en {HOST}:{PORT}")
-    message = 'Hola, servidor de eco'
+    message = input('Usuario: ')
     s.sendall(message.encode('utf-8'))
-    print(f"Enviado: {message}")
-    data = s.recv(1824)
 
-print(f"Recibido del servidor: {data.decode('utf-8')}")
+    response = s.recv(1824).decode('utf-8')
+    print(f"Recibido del servidor: {response}")
+    while response == 'si':
+      command = input("Dame un comando a mandar:")
+      s.sendall(command.encode('utf-8'))
+      if command == 'quit':
+        print('cerrado localmente')
+        response == ''
+        s.close()
+        break
+    print('Parece que se cerro la conexion')
+    s.close()
+
+# print(f"Recibido del servidor: {data.decode('utf-8')}")
--- server.py
+++ server.py
@@ -1,17 +1,53 @@
 import socket
-HOST = '127.0.0.1'
-PORT = 65432
+# HOST = '192.168.0.190'
+# HOST = '0.0.0.0'
+HOST = '192.168.100.79'
+PORT = 555
 
-while True:
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind((HOST, PORT))
-        s.listen()
-        print(f"Servidor escuchando en {HOST}.{PORT}")
-        conn, addr = s.accept()
-        with conn:
+fixed_users=[{'username':'badass420'},{'username':'mario123'}]
+isallowedconnection = True
+
+def get_is_valid_user(user_input)->bool:
+    for fixed_user in fixed_users:
+     if user_input == fixed_user['username']:
+       return True
+    return False
+
+# while True:
+    # with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    s.bind((HOST, PORT))
+    s.listen(5)
+    print(f"Servidor escuchando en {HOST}:{PORT}")
+
+    conn, addr = s.accept()
+    with conn:
+        entered_username = conn.recv(1024)
+        print(entered_username.decode('utf-8'))
+
+        if get_is_valid_user(entered_username.decode('utf-8')):
+            print('Asi es, si existe')
             print(f"Conectado por {addr}")
-            while True:
+            conn.sendall('si'.encode('utf-8'))
+
+            while isallowedconnection:
+                print('Escuchando clientes...')
                 data = conn.recv(1024)
+                print(f"Recibido: {data.decode('utf-8')}")
+                if data.decode('utf-8') == 'quit':
+                    print('Cerrando conexion')
+                    # conn.sendall('Cerrando conexion'.encode('utf-8'))
+                    conn.close()
+                    isallowedconnection = not isallowedconnection
                 if not data:
+                    print('Creo que aqui se cierra cuando el host se desconecta')
+                    conn.close()
                     break
-                conn.sendall(data)
+
+        else:
+            print('Parece que tu usuario no existe')
+        #    conn.sendall('Parece que tu usuario no existe'.encode('utf-8'))
+
+        conn.close()
+    conn.close()
